b56179583f1059a2ffa921b01cebec43c6be77bd
郭凡 <g39088902@gmail.com>
2022-03-23 14:33:13 +0800

    上canary

--- common/src/main/java/org/unicorn/whiteboard/common/http/Constant.kt
+++ common/src/main/java/org/unicorn/whiteboard/common/http/Constant.kt
@@ -2,13 +2,12 @@ package org.unicorn.whiteboard.common.http
 
 object Constant {
 
-    const val ENV = "dev"
-    const val AUENV = "dev" // autumn使用的环境
+    const val ENV = "canary"
+    const val AUENV = "prod" // autumn使用的环境
     const val BASE_URL = "https://$ENV.unicorn.org.cn/education/api/"
     const val OSS_URL = "https://$ENV.unicorn.org.cn/minioimg/"
     const val ODS_URL = "https://$AUENV.unicorn.org.cn/ods/api/"
 
-    //const val COGNITO_URL = "https://$ENV.unicorn.org.cn/cognito/api/"
-    const val COGNITO_URL = "https://dev.unicorn.org.cn/cognito/api/"
+    const val COGNITO_URL = "https://$ENV.unicorn.org.cn/cognito/api/"
 
 }
